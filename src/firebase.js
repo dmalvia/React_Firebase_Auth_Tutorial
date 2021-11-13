ebe8eaabdc55f7111d50db2d5bd9585e2ac64cb6
Dipesh Malvia <cs.dipeshmalvia@gmail.com>
2021-11-13 23:34:48 +0800

    Firebase configuration changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCG39BbXvUfivdkQv5KFh91gDC8AQpUBlE",
-  authDomain: "reactauthentication-baaba.firebaseapp.com",
-  projectId: "reactauthentication-baaba",
-  storageBucket: "reactauthentication-baaba.appspot.com",
-  messagingSenderId: "496879194154",
-  appId: "1:496879194154:web:c9102295f44fd72bd6b353",
+  apiKey: <YOURAPIKEY>,
+  authDomain: <YOURAUTHDOAMIN>,
+  projectId: <YOURPROJECTID>,
+  storageBucket: <YOURSTORAGEBUCKET>,
+  messagingSenderId: <YOURMESSAGINGSENDERID>,
+  appId: <YOURAPPID>,
 };
 
 // Initialize Firebase
