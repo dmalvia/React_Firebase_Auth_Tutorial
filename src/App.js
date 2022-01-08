7f03a0a9e163858cdce3b4a8a6c542549a1128b4
Dipesh Malvia <cs.dipeshmalvia@gmail.com>
2022-01-08 14:33:05 +0530

    phone auth

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import PhoneSignUp from "./components/PhoneSignUp";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { UserAuthContextProvider } from "./context/UserAuthContext";
 
@@ -24,6 +25,7 @@ function App() {
               />
               <Route path="/" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
+              <Route path="/phonesignup" element={<PhoneSignUp />} />
             </Routes>
           </UserAuthContextProvider>
         </Col>
--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -3,8 +3,6 @@ import { Navigate } from "react-router-dom";
 import { useUserAuth } from "../context/UserAuthContext";
 const ProtectedRoute = ({ children }) => {
   const { user } = useUserAuth();
-
-  console.log("Check user in Private: ", user);
   if (!user) {
     return <Navigate to="/" />;
   }
