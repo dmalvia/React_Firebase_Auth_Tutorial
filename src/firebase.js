b0871dddf61c6b08e8c98eeaa4477fabcc57e83c
Dipesh Malvia <cs.dipeshmalvia@gmail.com>
2022-01-09 15:51:16 +0530

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSobRSmQo7pMikYW2o9Pxz9Ik4Ta6W8eY",
-  authDomain: "phone-auth-84547.firebaseapp.com",
-  projectId: "phone-auth-84547",
-  storageBucket: "phone-auth-84547.appspot.com",
-  messagingSenderId: "256324206160",
-  appId: "1:256324206160:web:a03df6c891070867585b55",
+  apiKey: <YOURAPIKEY>,
+  authDomain: <YOURAUTHDOAMIN>,
+  projectId: <YOURPROJECTID>,
+  storageBucket: <YOURSTORAGEBUCKET>,
+  messagingSenderId: <YOURMESSAGINGSENDERID>,
+  appId: <YOURAPPID>,
 };
 
 // Initialize Firebase
